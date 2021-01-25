e5fcb8d6d95cf1aeb776fda756db21bd6c572789
Awais Mughal <iamawaisakram@gmail.com>
2021-01-25 10:37:59 +0500

    Changing notes for 1-some-functions-simple

--- __test__/1-some-functions-simple.test.js
+++ __test__/1-some-functions-simple.test.js
@@ -17,6 +17,6 @@ test("1-some-functions-simple testing reverseArray", function () {
   expect(someFunctionOj.reverseArray([1, 2, 3])).toEqual([3, 2, 1]);
 });
 
-//NOTES:
-// toEqual -> for both non-primitive and primitive - order of properties of object in toEqual doesn't matter, but of array it does (of-course)
-// toBe -> only works for primitive values
+// ! NOTES:
+// * toEqual -> for both non-primitive and primitive - order of properties of object in toEqual doesn't matter(because there's no index involved like that of an array), but if you use array, the order does matter (of-course)
+// * toBe -> only works for primitive values
